c9ab3817bbe752536c24107d1a5af0ef31a0246d
MichalBoczula <michal.boczula@gmail.com>
2018-08-23 21:47:36 +0200

    task 18.2

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -1,12 +1,54 @@
 package com.crud.tasks.trello.client;
 
+import com.crud.tasks.domain.TrelloBoardDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class TrelloClient {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
+
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+
+    @Value("${trello.app.token}")
+    private String trelloToken;
+
+    @Value("${trello.app.client}")
+    private String trelloClient;
+
+    public List<TrelloBoardDto> getTrelloBoards() {
+        final URI url = createURL();
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+
+        List<TrelloBoardDto> boardsList = Arrays.asList((boardsResponse)).stream()
+                .filter(trelloBoardDto -> trelloBoardDto.getId() != null)
+                .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla") == true)
+                .collect(Collectors.toList());
+        return Optional.ofNullable(boardsList).orElse(null);
+    }
+
+    private URI createURL() {
+        final URI localURL = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloClient + "/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("fields", "name,id").build().encode().toUri();
+        return localURL;
+    }
+
 }
