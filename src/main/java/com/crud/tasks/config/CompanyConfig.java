5d0f419f83c1f7c3e2420db3680798a695c7adfc
MichalBoczula <michal.boczula@gmail.com>
2018-10-01 18:40:18 +0200

    task 2 mod 24

--- src/main/java/com/crud/tasks/config/CompanyConfig.java
+++ src/main/java/com/crud/tasks/config/CompanyConfig.java
@@ -4,11 +4,9 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
 @Getter
-public class AdminConfig {
-    @Value("${admin.mail}")
-    private String adminMail;
-    @Value("${admin.name}")
-    private String adminName;
+@Component
+public class CompanyConfig {
+    @Value("${company.name}")
+    private String companyName;
 }
